f1bbcfa3411ade3fad8b3a81e362b4ac7b56732d
Jingchao Zhou <zhoujc999@gmail.com>
2020-07-23 23:58:52 +0000

    Bug fix

--- notes-app-api/src/libs/dynamoDB.js
+++ notes-app-api/src/libs/dynamoDB.js
@@ -2,25 +2,25 @@ import AWS from "aws-sdk";
 
 export const dynamoDBGet = (params) => {
   const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
-  dynamoDBClient.get(params).promise();
+  return dynamoDBClient.get(params).promise();
 };
 
 export const dynamoDBPut = (params) => {
   const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
-  dynamoDBClient.put(params).promise();
+  return dynamoDBClient.put(params).promise();
 };
 
 export const dynamoDBQuery = (params) => {
   const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
-  dynamoDBClient.query(params).promise();
+  return dynamoDBClient.query(params).promise();
 };
 
 export const dynamoDBUpdate = (params) => {
   const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
-  dynamoDBClient.update(params).promise();
+  return dynamoDBClient.update(params).promise();
 };
 
 export const dynamoDBDelete = (params) => {
   const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
-  dynamoDBClient.delete(params).promise();
+  return dynamoDBClient.delete(params).promise();
 };
